f68dc55c0f5ae515af6dc053a3c3a57f579d1021
mactunechy <mactunechy@gmail.com>
2020-11-25 20:36:57 +0200

    fixies:

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,10 @@ import { create } from 'apisauce';
 import AuthStore from './authStore';
 
 const apiClient = create({
-    baseURL: 'http://localhost:1337',
+    baseURL:
+        process.env.NODE_ENV === 'production'
+            ? 'http://rocket-miners.herokuapp.com'
+            : 'http://localhost:1337',
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
